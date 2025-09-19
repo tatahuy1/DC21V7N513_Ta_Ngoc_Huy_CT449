3f8fff84d41e1792a5caebad18ea0ca54c33bf47
Tên của bạn <email@example.com>
2025-09-20 00:52:17 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -10,5 +10,13 @@ app.get("/", (req, res) => {
 
 });
 app.use("/api/contacts", contactRoutes);
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+app.use ((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json ({
+        message: err.message || "Internal Server Error",
+    });
+});
 
 module.exports = app;
